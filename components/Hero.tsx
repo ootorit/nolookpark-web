32f07df08c57c399044be76f7b5b61c18352c236
澤田智洋 <266604138+yuru-creator@users.noreply.github.com>
2026-08-23 21:33:29 +0900

    ヒーロー下のスクロールヒントを削除

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -107,15 +107,6 @@ export default function Hero() {
           </div>
         </Reveal>
       </div>
-
-      {/* Scroll hint */}
-      <div
-        aria-hidden
-        className="pointer-events-none absolute bottom-8 left-1/2 flex -translate-x-1/2 flex-col items-center gap-2"
-      >
-        <span className="font-en text-[10px] tracking-[1px] text-ink">SCROLL</span>
-        <span className="h-8 w-px bg-ink/40" />
-      </div>
     </section>
   );
 }
